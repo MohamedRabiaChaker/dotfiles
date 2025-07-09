ffc4ddf47f27b9be8a345cf47bbf64367053d3ca
mohamed.chaker <mohamed.chaker@ataccama.com>
2025-07-09 16:38:42 +0100

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,34 @@
+require("core.keymaps")
+require("core.options")
+-- [[ Install `.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+	require("plugins.neotree"),
+	require("plugins.colortheme"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformatting"),
+	require("plugins.gitsigns"),
+	require("plugins.alpha"),
+	require("plugins.indentblankline"),
+	require("plugins.nvterm"),
+	require("plugins.copilot"),
+})
